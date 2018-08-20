6fdb99c4ebc2bd790fb95e52258bf649b17c3292
David Gao <davidgjm@gmail.com>
2018-08-21 04:55:44 +0800

    Initial project assets

---
+++ src/main/java/com/tng/oss/classdiagram/ClassDiagramApplication.java
@@ -0,0 +1,12 @@
+package com.tng.oss.classdiagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassDiagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassDiagramApplication.class, args);
+    }
+}
